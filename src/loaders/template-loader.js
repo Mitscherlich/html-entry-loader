c7e01efbf49ea5e94c7b4e21a3842d49b639e929
Mitscherlich <mitscherlich36@gmail.com>
2021-04-09 11:09:39 +0800

    pref: emit resolved html content instead of lodash template function

--- src/loaders/template-loader.js
+++ src/loaders/template-loader.js
@@ -1,10 +1,7 @@
-import _ from 'lodash';
 import { compile } from '../codegen/parser';
 import { normalizeOptions } from '../utils';
 
 import schema from '../options.json';
-
-const lodashRequire = require.resolve('lodash');
 
 module.exports = async function (source) {
   const loaderContext = this;
@@ -25,16 +22,5 @@ module.exports = async function (source) {
     loaderContext.emit(error instanceof Error ? error : new Error(error));
   }
 
-  // The following part renders the template with lodash as a minimalistic loader
-  const template = _.template(descriptor.html, {
-    interpolate: /<%=([\s\S]+?)%>/g,
-    variable: 'data',
-    ...options,
-  });
-
-  // Use __non_webpack_require__ to enforce using the native nodejs require
-  // during template execution
-  return `var _ = __non_webpack_require__(${JSON.stringify(lodashRequire)});
-// Execute the lodash template
-module.exports = (${template.source})();`;
+  return `module.exports = ${JSON.stringify(descriptor.html)}`;
 };
